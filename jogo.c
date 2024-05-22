a0b4d65b6c4a7d6b84ae073e670ec3d361001955
MarioJrr1 <163215916+MarioJrr1@users.noreply.github.com>
2024-05-21 22:23:41 -0300

    Update jogo.c

--- jogo.c
+++ jogo.c
@@ -20,6 +20,7 @@ typedef struct _personagem{
     int magia;
 } PERS;
 
+int facilita(char *hab, int *pontos, PERS personagem);
 void distribuir(int pontos, PERS *personagem);
 
 
@@ -44,6 +45,7 @@ int main()
         printf("Digite o nome do seu personagem: ");
         scanf("%s", personagem.nome);
         distribuir(pontos, &personagem);
+        //personagem.forca = numaleatorio;
         printf("%s %d %d %d %d %d", personagem.nome, personagem.forca, personagem.defesa, personagem.magia, personagem.folego, personagem.carisma);
     }
     
@@ -52,75 +54,36 @@ int main()
 
 void distribuir(int pontos, PERS *personagem){
     printf("Voce tem %d pontos. Distribua-os da maneira que quiser\n", pontos);
-    printf("Forca: %d\nDefesa: %d\nMagia: %d\nFolego: %d\nCarisma: %d", personagem->forca, personagem->defesa, personagem->magia, personagem->folego, personagem->carisma);
+    int pontos2 = pontos;
+    char forca[10] = "forca";
+    char defesa[10] = "defesa";
+    char magia[10] = "magia";
+    char folego[10] = "folego";
+    char carisma[10] = "carisma";
+    personagem->forca = facilita(forca, &pontos2, *personagem);
+    personagem->defesa = facilita(defesa, &pontos2, *personagem);
+    personagem->magia = facilita(magia, &pontos2, *personagem);
+    personagem->folego = facilita(folego, &pontos2, *personagem);
+    personagem->carisma = facilita(carisma, &pontos2, *personagem);
+
+
+}
+
+int facilita(char *hab, int *pontos, PERS personagem){
+
+    printf("Forca: %d\nDefesa: %d\nMagia: %d\nFolego: %d\nCarisma: %d", personagem.forca, personagem.defesa, personagem.magia, personagem.folego, personagem.carisma);
     printf("\n");
+    int x;
     while(1){
-        printf("Pontos disponiveis %d\nAtribua os valores de forca a seguir: ", pontos);
-        scanf("%d", &personagem->forca);
-        if(personagem->forca > pontos || personagem->forca < 0){
+        printf("Pontos disponiveis %d\nAtribua os valores de %s a seguir: ", *pontos, hab);
+        scanf("%d", &x);
+        if(x > *pontos || x < 0){
             printf("Voce nao tem pontos suficientes\n");
             continue;
         }
         else
             break;
     }
-    
-    pontos = pontos - personagem->forca;
-
-    printf("Forca: %d\nDefesa: %d\nMagia: %d\nFolego: %d\nCarisma: %d", personagem->forca, personagem->defesa, personagem->magia, personagem->folego, personagem->carisma);
-    printf("\n");
-    while(1){
-        printf("Pontos disponiveis %d\nAtribua os valores de defesa a seguir: ", pontos);
-        scanf("%d", &personagem->defesa);
-        if(personagem->defesa > pontos || personagem->defesa < 0){
-            printf("Voce nao tem pontos suficientes\n");
-            continue;
-        }
-        else
-            break;
-    }
-    pontos = pontos - personagem->defesa;
-
-    printf("Forca: %d\nDefesa: %d\nMagia: %d\nFolego: %d\nCarisma: %d", personagem->forca, personagem->defesa, personagem->magia, personagem->folego, personagem->carisma);
-    printf("\n");
-    while(1){
-        printf("Pontos disponiveis %d\nAtribua os valores de magia a seguir: ", pontos);
-        scanf("%d", &personagem->magia);
-        if(personagem->magia > pontos || personagem->magia < 0){
-            printf("Voce nao tem pontos suficientes\n");
-            continue;
-        }
-        else
-            break;
-    }
-    pontos = pontos - personagem->magia;
-
-    printf("Forca: %d\nDefesa: %d\nMagia: %d\nFolego: %d\nCarisma: %d", personagem->forca, personagem->defesa, personagem->magia, personagem->folego, personagem->carisma);
-    printf("\n");
-    while(1){
-        printf("Pontos disponiveis %d\nAtribua os valores de folego a seguir: ", pontos);
-        scanf("%d", &personagem->folego);
-        if(personagem->folego > pontos || personagem->folego < 0){
-            printf("Voce nao tem pontos suficientes\n");
-            continue;
-        }
-        else
-            break;
-    }
-    pontos = pontos - personagem->folego;
-
-    printf("Forca: %d\nDefesa: %d\nMagia: %d\nFolego: %d\nCarisma: %d", personagem->forca, personagem->defesa, personagem->magia, personagem->folego, personagem->carisma);
-    printf("\n");
-    while(1){
-        printf("Pontos disponiveis %d\nAtribua os valores de carisma a seguir: ", pontos);
-        scanf("%d", &personagem->carisma);
-        if(personagem->carisma > pontos || personagem->carisma < 0){
-            printf("Voce nao tem pontos suficientes\n");
-            continue;
-        }
-        else
-            break;
-    }
-    pontos = pontos - personagem->carisma;
-
+    *pontos = *pontos - x;
+    return x;
 }
